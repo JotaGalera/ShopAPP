63a1b324519d6bde659cb1d4517fa65dfaa34e35
Javier Galera <javier.galera@bestsecret.com>
2020-03-05 20:38:20 +0100

    Closed #5 getProductListUseCase unit test created

---
+++ ShopAPP/ShopAPPTests/getProductsList.swift
@@ -0,0 +1,30 @@
+//
+//  getProductsList.swift
+//  ShopAPPTests
+//
+//  Created by Javier Galera Garrido on 05/03/2020.
+//  Copyright © 2020 Javier Galera Garrido. All rights reserved.
+//
+
+import XCTest
+@testable import ShopAPP
+
+class getProductsList: XCTestCase {
+
+    var sut : GetProductListUseCase?
+    
+    override func setUp() {
+        
+    }
+
+    override func tearDown() {
+        
+    }
+
+    func testExample() {
+        
+    }
+
+    
+
+}
