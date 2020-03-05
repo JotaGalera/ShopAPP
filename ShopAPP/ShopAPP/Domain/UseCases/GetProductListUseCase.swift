260757649d328abe147109af5751210fb4c4b31b
Javier Galera <javier.galera@bestsecret.com>
2020-03-05 20:39:52 +0100

    Closed #6 Pass the test with the new entity

---
+++ ShopAPP/ShopAPP/Domain/UseCases/GetProductListUseCase.swift
@@ -0,0 +1,17 @@
+//
+//  GetProductListUseCase.swift
+//  ShopAPP
+//
+//  Created by Javier Galera Garrido on 05/03/2020.
+//  Copyright © 2020 Javier Galera Garrido. All rights reserved.
+//
+
+import Foundation
+
+protocol GetProductListUseCase {
+
+}
+
+class GetProductListUseCaseImplementation : GetProductListUseCase {
+    
+}
