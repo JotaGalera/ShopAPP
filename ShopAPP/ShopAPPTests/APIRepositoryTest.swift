ff062d1a91307c0f70a3778974c86a0c42c19b9c
Javier Galera <javier.galera@bestsecret.com>
2020-03-06 15:36:17 +0100

    Closed #12 APIRepository test (DATA layer)

---
+++ ShopAPP/ShopAPPTests/APIRepositoryTest.swift
@@ -0,0 +1,16 @@
+//
+//  APIRepositoryTest.swift
+//  ShopAPPTests
+//
+//  Created by Galera, Javier on 06/03/2020.
+//  Copyright © 2020 Javier Galera Garrido. All rights reserved.
+//
+
+import XCTest
+@testable import ShopAPP
+
+class APIRepositoryTest: XCTestCase {
+
+    var sut : APIRepository?
+    
+}
