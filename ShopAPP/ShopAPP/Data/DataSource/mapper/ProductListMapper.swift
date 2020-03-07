a80cef59b74d629f3418e7ef937dddb67abb23f2
Javier Galera <javier.galera@bestsecret.com>
2020-03-08 00:57:28 +0100

    Closed #24 ProductListMapper entity

---
+++ ShopAPP/ShopAPP/Data/DataSource/mapper/ProductListMapper.swift
@@ -0,0 +1,15 @@
+//
+//  ProductListMapper.swift
+//  ShopAPP
+//
+//  Created by Javier Galera Garrido on 08/03/2020.
+//  Copyright © 2020 Javier Galera Garrido. All rights reserved.
+//
+
+import Foundation
+
+class ProductListMapper {
+    func convert(data: Data) -> ProductListData{
+        
+    }
+}
