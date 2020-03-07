7db19c39372c9bfbe1acb0ae91ff251545e4913f
Javier Galera <javier.galera@bestsecret.com>
2020-03-07 16:37:43 +0100

    Closed #13 APIRepositoryImplementation in Data layer

---
+++ ShopAPP/ShopAPP/Data/Repository/APIRepositoryImplementation.swift
@@ -0,0 +1,15 @@
+//
+//  APIRepositoryImplementation.swift
+//  ShopAPP
+//
+//  Created by Javier Galera Garrido on 07/03/2020.
+//  Copyright © 2020 Javier Galera Garrido. All rights reserved.
+//
+
+import Foundation
+
+class APIRepositoryImplementation : APIRepository {
+    func getProductList(completion: @escaping (ProductList) -> ()) {
+        
+    }
+}
