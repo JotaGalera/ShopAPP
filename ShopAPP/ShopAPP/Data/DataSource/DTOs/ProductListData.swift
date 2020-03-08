439f9bc68ad0290cd9eb748c89257e99cf432602
Javier Galera <javier.galera@bestsecret.com>
2020-03-08 01:07:43 +0100

    Closed #25 ProductListData created

---
+++ ShopAPP/ShopAPP/Data/DataSource/DTOs/ProductListData.swift
@@ -0,0 +1,13 @@
+//
+//  ProductListData.swift
+//  ShopAPP
+//
+//  Created by Javier Galera Garrido on 08/03/2020.
+//  Copyright © 2020 Javier Galera Garrido. All rights reserved.
+//
+
+import Foundation
+
+class ProductListData {
+    
+}
