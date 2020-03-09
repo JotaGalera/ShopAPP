5d737ee670403c092d7c2cd2507c2ea6afb4dfbc
Javier Galera <javier.galera@bestsecret.com>
2020-03-09 21:03:20 +0100

    Closed #37 iterate ProductListPresenter test and entity

--- ShopAPP/ShopAPP/Design/Features/ProductList/Scene/ProductListPresenter.swift
+++ ShopAPP/ShopAPP/Design/Features/ProductList/Scene/ProductListPresenter.swift
@@ -1,5 +1,19 @@
 import Foundation
 
 protocol ProductListPresenter {
+     func getProductList()
+}
+
+class ProductListPresenterImplementation : ProductListPresenter {
+    private var getProductListUseCase: GetProductListUseCase
     
+    init( getProductListUseCase: GetProductListUseCase = GetProductListUseCaseImplementation() ){
+        self.getProductListUseCase = getProductListUseCase
+    }
+    
+    func getProductList(){
+        self.getProductListUseCase.execute(completion: { productList in
+            print(productList)
+        })
+    }
 }
--- ShopAPP/ShopAPPTests/ProductListPresenterTest.swift
+++ ShopAPP/ShopAPPTests/ProductListPresenterTest.swift
@@ -1,20 +1,33 @@
 import XCTest
+@testable import ShopAPP
 
 class ProductListPresenterTest: XCTestCase {
 
     var sut : ProductListPresenter?
+    fileprivate let mockedGetProductListUseCaseImplementation = MockedGetProductListUseCaseImplementation()
     
     override func setUp() {
-        
+        super.setUp()
+        sut = ProductListPresenterImplementation(getProductListUseCase: mockedGetProductListUseCaseImplementation)
     }
 
     override func tearDown() {
-        
+        sut = nil
+        super.tearDown()
     }
 
-    func testExample() {
+    func testGetData() {
+        sut?.getProductList()
         
+        XCTAssertEqual(mockedGetProductListUseCaseImplementation.called, true)
     }
 
     
+}
+fileprivate class MockedGetProductListUseCaseImplementation : GetProductListUseCaseImplementation {
+    var called = false
+    
+    override func execute(completion: @escaping (ProductList) -> ()) {
+        self.called = true
+    }
 }
