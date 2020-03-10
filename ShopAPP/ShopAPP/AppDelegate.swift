d01f241f6c9dc29265f5fa98f0e568eecb54796a
Javier Galera <javier.galera@bestsecret.com>
2020-03-10 16:59:45 +0100

    Uploading initial XIB

--- ShopAPP/ShopAPP/AppDelegate.swift
+++ ShopAPP/ShopAPP/AppDelegate.swift
@@ -5,6 +5,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+    
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+      return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+    
+    /*func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         setInitialViewController()
         return true
     }
@@ -14,7 +22,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         let navigationController = UINavigationController(rootViewController: productListViewController)
         window?.rootViewController = navigationController
         window?.makeKeyAndVisible()
-    }
+    }*/
     
 }
 
---
+++ ShopAPP/ShopAPP/Design/Features/ProductList/Scene/APIDataSourceConfig.swift
@@ -0,0 +1,9 @@
+//
+//  APIDataSourceConfig.swift
+//  ShopAPP
+//
+//  Created by Galera, Javier on 10/03/2020.
+//  Copyright © 2020 Javier Galera Garrido. All rights reserved.
+//
+
+import Foundation
